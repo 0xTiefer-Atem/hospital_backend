516e7429ec33caf6a580818da3a82aaab07cea68
0xMilk <1144502582@qq.com>
2020-02-16 00:22:10 +0800

    修改预约dao层操作

--- src/main/java/org/hospital/management/dao/AppointmentDao.java
+++ src/main/java/org/hospital/management/dao/AppointmentDao.java
@@ -1,5 +1,6 @@
 package org.hospital.management.dao;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.hospital.management.pojo.AppointmentPojo;
@@ -8,21 +9,31 @@ import java.util.List;
 
 @Mapper
 public interface AppointmentDao {
-    @Select("select distinct ap.appointmentId,uI.userName,sI.staffName,ap.appointmentTime" +
-            ",cI.cliName" +
-            " from appointmentInfo ap join userInfo uI on ap.userId = uI.userId" +
-            " join staffInfo sI on ap.staffId = sI.staffId" +
-            " join clinicInfo cI")
+    @Select("select distinct ap.appointmentId," +
+            "ap.userId," +
+            "uI.userName," +
+            "sI.staffId," +
+            "sI.staffName," +
+            "ap.appointmentTime," +
+            "cI.cliName " +
+            "from appointmentInfo ap," +
+            "userInfo uI," +
+            "staffInfo sI," +
+            "clinicInfo cI" +
+            " where ap.userId = uI.userId" +
+            " and ap.staffId = sI.staffId" +
+            " and ap.staffId like concat(cI.cliId,'%')")
     List<AppointmentPojo> appointmentListInit();
 
 
     @Select("select distinct ap.appointmentId," +
             "ap.userId," +
             "uI.userName," +
+            "sI.staffId," +
             "sI.staffName," +
             "ap.appointmentTime," +
-            "cI.cliName" +
-            " from appointmentInfo ap," +
+            "cI.cliName " +
+            "from appointmentInfo ap," +
             "userInfo uI," +
             "staffInfo sI," +
             "clinicInfo cI" +
