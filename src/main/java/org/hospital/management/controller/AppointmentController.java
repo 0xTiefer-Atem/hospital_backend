247068b30b3c3f5743ca5c52c68dc0ff2c5876ef
0xMilk <1144502582@qq.com>
2020-02-15 22:53:28 +0800

    编写预约接口

--- src/main/java/org/hospital/management/controller/AppointmentController.java
+++ src/main/java/org/hospital/management/controller/AppointmentController.java
@@ -1,18 +1,41 @@
 package org.hospital.management.controller;
 
-import org.springframework.http.HttpRequest;
+import org.hospital.management.dao.AppointmentDao;
+import org.hospital.management.pojo.AppointmentPojo;
+import org.hospital.management.util.ResponseHelper;
+import org.hospital.management.util.ResponseV2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "/home/appointment")
+@CrossOrigin
 public class AppointmentController {
+    @Autowired
+    AppointmentDao appointmentDao;
+
 
     @RequestMapping(value = "/appointmentListInit", method = RequestMethod.GET)
     @ResponseBody
-    public String response() {
-        return "哈哈哈哈";
+    public ResponseV2 appointmentListInit() {
+        List<AppointmentPojo> appointmentList = appointmentDao.appointmentListInit();
+        return ResponseHelper.create(appointmentList, 200, "预约队列查询成功");
+    }
+
+
+    @RequestMapping(value = "/searchByAppointmentId", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseV2 searchByAppointmentId(HttpServletRequest request) {
+        String appointmentId = request.getParameter("appointmentId");
+        System.out.println(appointmentId);
+        List<AppointmentPojo> appointmentPojoList = appointmentDao.searchAppointmentById(appointmentId);
+        return ResponseHelper.create(appointmentPojoList, 200, "查询预约号成功");
     }
 }
--- src/main/java/org/hospital/management/dao/AppointmentDao.java
+++ src/main/java/org/hospital/management/dao/AppointmentDao.java
@@ -8,6 +8,27 @@ import java.util.List;
 
 @Mapper
 public interface AppointmentDao {
-    @Select("")
+    @Select("select distinct ap.appointmentId,uI.userName,sI.staffName,ap.appointmentTime" +
+            ",cI.cliName" +
+            " from appointmentInfo ap join userInfo uI on ap.userId = uI.userId" +
+            " join staffInfo sI on ap.staffId = sI.staffId" +
+            " join clinicInfo cI")
     List<AppointmentPojo> appointmentListInit();
+
+
+    @Select("select distinct ap.appointmentId," +
+            "ap.userId," +
+            "uI.userName," +
+            "sI.staffName," +
+            "ap.appointmentTime," +
+            "cI.cliName" +
+            " from appointmentInfo ap," +
+            "userInfo uI," +
+            "staffInfo sI," +
+            "clinicInfo cI" +
+            " where ap.userId = uI.userId" +
+            " and ap.staffId = sI.staffId" +
+            " and appointmentId = #{id}" +
+            " and ap.staffId like concat(cI.cliId,'%')")
+    List<AppointmentPojo> searchAppointmentById(String id);
 }
