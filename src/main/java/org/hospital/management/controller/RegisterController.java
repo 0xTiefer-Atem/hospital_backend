e9d6d074ca208f98c1504bea4bb2bd2f0828889a
0xMilk <1144502582@qq.com>
2020-02-16 16:35:49 +0800

    编写registerById功能

--- src/main/java/org/hospital/management/controller/RegisterController.java
+++ src/main/java/org/hospital/management/controller/RegisterController.java
@@ -1,10 +1,12 @@
 package org.hospital.management.controller;
 
+import org.hospital.management.dao.RegisterDao;
 import org.hospital.management.pojo.RegisterPojo;
 import org.hospital.management.util.GetUUID;
 import org.hospital.management.util.ResponseHelper;
 import org.hospital.management.util.ResponseV2;
 import org.hospital.management.util.TimeOpt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping(value = "/home/register")
 @CrossOrigin
 public class RegisterController {
+    @Autowired
+    RegisterDao registerDao;
 
     @RequestMapping(value = "/registerById", method = RequestMethod.POST)
     @ResponseBody
@@ -32,6 +36,12 @@ public class RegisterController {
         registerPojo.setUserId(userId);
         registerPojo.setCreateTime(TimeOpt.getCurrentTime());
 
-        return ResponseHelper.create();
+        try{
+            registerDao.insertRegister(registerPojo);
+        }catch (Exception e){
+            System.out.println(e);
+            return ResponseHelper.create(500, "挂号操作失败");
+        }
+        return ResponseHelper.create(200,"挂号操作失败");
     }
 }
