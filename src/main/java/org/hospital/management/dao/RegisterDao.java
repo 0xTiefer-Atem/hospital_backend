04365527b55f89680a7c8656eb4c09c3ad17c3f3
0xMilk <1144502582@qq.com>
2020-02-17 15:00:23 +0800

    修改insert语句里的部分变量名字，添加初始化挂号队列语句

--- src/main/java/org/hospital/management/dao/RegisterDao.java
+++ src/main/java/org/hospital/management/dao/RegisterDao.java
@@ -2,16 +2,32 @@ package org.hospital.management.dao;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import org.hospital.management.pojo.RegisterPojo;
 
+import java.util.List;
+
 @Mapper
 public interface RegisterDao {
-    @Insert("insert into register(registerId, appointmentId, staffId, " +
-            "userId, createdTime) values(#{registerId}, #{appointmentId}," +
-            "#{staffId}, #{userId}, #{createdTime})")
+    @Insert("insert into registerInfo(registerId, appointmentId, staffId, " +
+            "userId, createTime) values(#{registerId}, #{appointmentId}," +
+            "#{staffId}, #{userId}, #{createTime})")
     void insertRegister(RegisterPojo r);
 
     @Update("update appointmentInfo set status = 'SUCCESS' where appointmentId = #{id}")
     void upDateAppointmentStatus(String id);
+
+    @Select("select distinct  rI.appointmentId, rI.registerId, rI.userId, uI.userName," +
+            "                rI.staffId, sI.staffName, cI.cliName, rI.createTime" +
+            " from registerInfo rI," +
+            "     userInfo uI," +
+            "     staffInfo sI," +
+            "     clinicInfo cI" +
+            " where rI.userId = uI.userId" +
+            "  and rI.staffId = sI.staffId" +
+            "  and rI.staffId = #{staffId}" +
+            "  and rI.staffId like concat(cI.cliId,'%')" +
+            "  and rI.createTime between #{startTime} and #{endTime}")
+    List<RegisterPojo> registerListInit(String staffId, String startTime, String endTime);
 }
