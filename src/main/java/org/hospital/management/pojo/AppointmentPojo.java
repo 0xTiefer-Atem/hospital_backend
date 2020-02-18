9090b7a84e79441fe720bb011b0ddac8832abb91
0xMilk <1144502582@qq.com>
2020-02-18 15:58:33 +0800

    为AppointmentPojo类增加@ToString注解

--- src/main/java/org/hospital/management/pojo/AppointmentPojo.java
+++ src/main/java/org/hospital/management/pojo/AppointmentPojo.java
@@ -3,10 +3,12 @@ package org.hospital.management.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class AppointmentPojo {
     String appointmentId;
     String userId;
