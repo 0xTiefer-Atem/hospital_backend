3f0d820ffd1c2748bfb3ab36da9ef72031aee586
0xMilk <1144502582@qq.com>
2020-02-18 15:58:55 +0800

    为CasePojo类增加@ToString注解

--- src/main/java/org/hospital/management/pojo/CasePojo.java
+++ src/main/java/org/hospital/management/pojo/CasePojo.java
@@ -7,10 +7,12 @@ package org.hospital.management.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class CasePojo {
     private String caseId;
     private String registerId;
