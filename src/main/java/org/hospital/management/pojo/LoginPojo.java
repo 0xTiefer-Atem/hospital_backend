97a823282f7330925478ded8af8a72ea1529917c
0xMilk <1144502582@qq.com>
2020-02-18 16:58:24 +0800

    添加LoginPojo实体类

---
+++ src/main/java/org/hospital/management/pojo/LoginPojo.java
@@ -0,0 +1,14 @@
+package org.hospital.management.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class LoginPojo {
+    private String staffId;
+}
