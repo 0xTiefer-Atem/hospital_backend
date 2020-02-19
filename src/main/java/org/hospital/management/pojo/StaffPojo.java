e8608e3377ddec42051fb9794f9800a47486e6ce
0xMilk <1144502582@qq.com>
2020-02-19 16:47:21 +0800

    添加职员实体模型类

---
+++ src/main/java/org/hospital/management/pojo/StaffPojo.java
@@ -0,0 +1,20 @@
+package org.hospital.management.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class StaffPojo {
+    private String staffId;
+    private String staffName;
+    private String staffSex;
+    private String staffPos;
+    private String staffTel;
+    private String staffEntry;
+    private String createTime;
+}
