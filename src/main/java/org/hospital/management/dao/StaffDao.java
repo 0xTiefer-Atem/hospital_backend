9a1a253b5a1ccdb4f38fa1667796cb48483f4b3d
0xMilk <1144502582@qq.com>
2020-02-19 19:52:19 +0800

    编写查询删除职员sql语句

--- src/main/java/org/hospital/management/dao/StaffDao.java
+++ src/main/java/org/hospital/management/dao/StaffDao.java
@@ -1,5 +1,6 @@
 package org.hospital.management.dao;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.hospital.management.pojo.StaffPojo;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface StaffDao {
     @Select("select * from staffInfo")
     List<StaffPojo> staffListInit();
+
+    @Delete("delete from staffInfo where staffId = #{ID}")
+    void deleteStaffById(String id);
 }
