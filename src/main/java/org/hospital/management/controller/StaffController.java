814d4a8135d3abc098b9ba33409f00df54b237f0
0xMilk <1144502582@qq.com>
2020-02-19 20:23:59 +0800

    编写查询添加职员接口

--- src/main/java/org/hospital/management/controller/StaffController.java
+++ src/main/java/org/hospital/management/controller/StaffController.java
@@ -1,10 +1,13 @@
 package org.hospital.management.controller;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.hospital.management.dao.StaffDao;
 import org.hospital.management.pojo.StaffPojo;
 import org.hospital.management.util.ResponseHelper;
 import org.hospital.management.util.ResponseV2;
+import org.hospital.management.util.TimeOpt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -41,6 +44,39 @@ public class StaffController {
         }catch (Exception e){
             System.out.println(e.getMessage());
             return ResponseHelper.create(500, "职员删除失败!");
+        }
+    }
+
+
+    @RequestMapping(value = "/addStaff", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseV2 addStaff(@RequestBody Map paraMap) {
+        System.out.println(paraMap);
+        String staffInfo = (String) paraMap.get("staffInfo");
+        JSONObject jsonObject = JSON.parseObject(staffInfo);
+
+        String staffId = jsonObject.getString("staffId");
+        String staffName = jsonObject.getString("staffName");
+        String staffSex = jsonObject.getString("staffSex");
+        String staffPos = jsonObject.getString("staffPos");
+        String staffTel = jsonObject.getString("staffTel");
+        String staffEntry = jsonObject.getString("staffEntry");
+        String createTime = TimeOpt.getCurrentTime().split(" ")[0];
+
+        StaffPojo staffPojo = new StaffPojo();
+        staffPojo.setStaffId(staffId);
+        staffPojo.setStaffName(staffName);
+        staffPojo.setStaffSex(staffSex);
+        staffPojo.setStaffPos(staffPos);
+        staffPojo.setStaffTel(staffTel);
+        staffPojo.setStaffEntry(staffEntry);
+        staffPojo.setCreateTime(createTime);
+
+        try {
+            staffDao.addStaff(staffPojo);
+            return ResponseHelper.create(200, "新职员添加成功!");
+        }catch (Exception e) {
+            return ResponseHelper.create(500, "新职员添加失败!");
         }
     }
 }
