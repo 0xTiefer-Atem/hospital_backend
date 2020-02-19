9556753416d392dddb7f11118f10e291b3af1590
0xMilk <1144502582@qq.com>
2020-02-19 17:16:10 +0800

    编写查询所有职员接口

--- src/main/java/org/hospital/management/controller/StaffController.java
+++ src/main/java/org/hospital/management/controller/StaffController.java
@@ -2,6 +2,7 @@ package org.hospital.management.controller;
 
 
 import org.hospital.management.dao.StaffDao;
+import org.hospital.management.pojo.StaffPojo;
 import org.hospital.management.util.ResponseHelper;
 import org.hospital.management.util.ResponseV2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/home/staff")
@@ -17,9 +21,15 @@ public class StaffController {
 
     @Autowired
     StaffDao staffDao;
-    @RequestMapping(value = "/staffListInit", method = RequestMethod.GET)
-    public ResponseV2 staffListInit() {
 
-        return ResponseHelper.create(200, "");
+    @RequestMapping(value = "/staffListInit", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseV2 staffListInit() {
+        try {
+            List<StaffPojo> staffPojoList = staffDao.staffListInit();
+            return ResponseHelper.create(staffPojoList, 200, "职员信息队列初始化成功!");
+        }catch (Exception e){
+            return ResponseHelper.create(500, "职员信息队列初始化失败!");
+        }
     }
 }
