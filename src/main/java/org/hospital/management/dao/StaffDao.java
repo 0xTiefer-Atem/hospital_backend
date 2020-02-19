0e8bc9bac3e0a89e1ec1d8506988dccde4f5816f
0xMilk <1144502582@qq.com>
2020-02-19 17:15:51 +0800

    编写查询所有职员sql语句

--- src/main/java/org/hospital/management/dao/StaffDao.java
+++ src/main/java/org/hospital/management/dao/StaffDao.java
@@ -1,4 +1,13 @@
 package org.hospital.management.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.hospital.management.pojo.StaffPojo;
+
+import java.util.List;
+
+@Mapper
 public interface StaffDao {
+    @Select("select * from staffInfo")
+    List<StaffPojo> staffListInit();
 }
