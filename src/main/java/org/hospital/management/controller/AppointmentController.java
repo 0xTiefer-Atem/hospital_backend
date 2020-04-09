3a57e86dff331a8ca876006d4f1b5829146663ba
0xMilk <1144502582@qq.com>
2020-04-09 19:00:48 +0800

    注解改称Resource

--- src/main/java/org/hospital/management/controller/AppointmentController.java
+++ src/main/java/org/hospital/management/controller/AppointmentController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +18,7 @@ import java.util.Map;
 @RequestMapping(value = "/home/appointment")
 @CrossOrigin
 public class AppointmentController {
-    @Autowired
+    @Resource
     AppointmentDao appointmentDao;
 
 
