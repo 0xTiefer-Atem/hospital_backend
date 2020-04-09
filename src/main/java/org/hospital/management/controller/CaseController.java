04e8b81251dd0663017f4117fd41b4911ff0d910
0xMilk <1144502582@qq.com>
2020-04-09 19:00:58 +0800

    注解改称Resource

--- src/main/java/org/hospital/management/controller/CaseController.java
+++ src/main/java/org/hospital/management/controller/CaseController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
@@ -24,7 +25,7 @@ import java.util.Map;
 @RequestMapping(value = "/home/case")
 public class CaseController {
 
-    @Autowired
+    @Resource
     CaseDao caseDao;
 
     @RequestMapping(value = "/treatmentQueueInfoListInit", method = RequestMethod.POST)
